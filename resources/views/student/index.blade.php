197ad4637d42fd0082d3c86dc2d5915d54484e23
unknown <2023310508@pampangastateu.edu.ph>
2026-05-27 15:14:06 +0800

    inayos ko lang index

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -12,38 +12,56 @@
     </div>
     <!-- /.content-header -->
 
-<!-- Main content -->
+    <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-
-                    <div class="alert alert-info">
-                        This is the table for the students
-                    </div>
-
                     <div class="card">
-                        <div class="card-body p-0">
-
-                            <table class="table">
+                        <div class="card-header">
+                            <h3 class="card-title">Students List</h3>
+                            <div class="card-tools">
+                                <a href="{{ route('student.create') }}" class="btn btn-primary btn-sm">Add Student</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
                                 <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>First Name</th>
+                                        <th>Middle Name</th>
+                                        <th>Last Name</th>
+                                        <th>Address</th>
+                                        <th>Date of Birth</th>
+                                        <th>Actions</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                 {{-- @foreach($student) --}}
-                                    <tr>
-                                        <td>Matthew Bartolomeo</td>
-                                        <td>matthewbartolomeo@sample.com</td>
-                                    </tr>
-                                {{-- @endforeach --}}
+                                    @foreach($students as $student)
+                                        <tr>
+                                            <td>{{ $student->id }}</td>
+                                            <td>{{ $student->first_name }}</td>
+                                            <td>{{ $student->middle_name }}</td>
+                                            <td>{{ $student->last_name }}</td>
+                                            <td>{{ $student->address
+                                                 }}</td>
+                                            <td>{{ $student->dob }}</td>
+                                            <td>
+                                                <a href="{{ route('student.edit', $student->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                                <form action="{{ route('student.destroy', $student->id) }}" method="POST" style="display: inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
-                        <!-- /.card-body -->
                     </div>
-
                 </div>
             </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
+        </div>
     </div>
-@endsection
